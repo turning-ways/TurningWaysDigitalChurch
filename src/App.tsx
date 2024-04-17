9ccb0b9fff85fbdffeb9ee4ab2e03bfa3a1c427a
Temidire <temidireowoeye@gmail.com>
2024-04-17 11:28:27 +0100

    create routes

--- src/App.tsx
+++ src/App.tsx
@@ -11,7 +11,23 @@ import LoginWithNumber from "./pages/Signin/WithPhoneNumber/LoginWithNumber";
 import EmailForPasswordReset from "./pages/PasswordReset/EmailEntry/EmailForPasswordReset";
 import PasswordResetOtp from "./pages/PasswordReset/Otp/ForgotPassword/OtpVerification";
 import Navbar from "./components/Navbar/Navbar";
-import Dashboard from "./pages/Dashboard/Dashboard";
+import Dashboard from "./pages/Overview/Dashboard/Dashboard";
+import PersonalInformation from "./pages/Overview/Membership/Membership Profile/PersonalInformation";
+import ContactInformation from "./pages/Overview/Membership/Membership Profile/ContactInformation";
+import ChurchInformation from "./pages/Overview/Membership/Membership Profile/ChurchInformation";
+import ProfileEdit from "./pages/Overview/Membership/Edit Profile/ProfileEdit";
+import EditProfilePersonalInfo from "./pages/Overview/Membership/Edit Profile/EditProfilePersonalInfo";
+import EditProfileContactInfo from "./pages/Overview/Membership/Edit Profile/EditProfileContactInfo";
+import EditProfileChurchInfo from "./pages/Overview/Membership/Edit Profile/EditProfileChurchInformation";
+import MembershipProfile from "./pages/Overview/Membership/Membership Profile/MembershipProfile";
+import Membership from "./pages/Overview/Membership/Membership";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import Forms from "./pages/Overview/Forms/Forms";
+import Workflow from "./pages/Overview/Workflow/Workflow";
+import Settings from "./pages/Overview/Settings/Settings";
+import Help from "./pages/Overview/Help/Help";
+import Logout from "./pages/Overview/Logout/Logout";
 
 function App() {
   return (
@@ -26,6 +42,7 @@ function App() {
           <Route path="/personalinfo" element={<PersonalInfo />} />
           <Route path="/organizationinfo" element={<OrganizationInfo />} />
           <Route path="/churchinfo" element={<ChurchInfo />} />
+
           <Route
             path="/signup/otp-verification"
             element={<OtpVerification />}
@@ -43,8 +60,46 @@ function App() {
             element={<EmailForPasswordReset />}
           />
           <Route path="/overview/dashboard" element={<Dashboard />} />
+          <Route path="/overview/membership" element={<MembershipProfile />}>
+            <Route
+              path="personal-information"
+              element={<PersonalInformation />}
+            />
+            <Route
+              path="contact-information"
+              element={<ContactInformation />}
+            />
+            <Route path="church-information" element={<ChurchInformation />} />
+          </Route>
+          <Route
+            path="/overview/membership/profile-edit"
+            element={<ProfileEdit />}
+          >
+            <Route path="personal-info" element={<EditProfilePersonalInfo />} />
+            <Route path="contact-info" element={<EditProfileContactInfo />} />
+            <Route path="church-info" element={<EditProfileChurchInfo />} />
+          </Route>
+          <Route path="/members" element={<Membership />} />
+
+          {/* Forms Route */}
+          <Route path="/overview/forms" element={<Forms />} />
+
+          {/* Workflow Route */}
+          <Route path="/overview/workflow" element={<Workflow />} />
+
+          {/* Settings Route */}
+          <Route path="/overview/settings" element={<Settings />} />
+
+          {/* Help Route */}
+          <Route path="/overview/help" element={<Help />} />
+
+          {/* Logout Route */}
+          <Route path="/overview/logout" element={<Logout />} />
         </Routes>
+        {/* <Route path="/checkout">
+        </Route> */}
       </Router>
+      <ToastContainer />
     </>
   );
 }
