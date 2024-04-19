e1b62cd9684bf7b6d5bdb3f2bd3dab08659b782b
Temidire <temidireowoeye@gmail.com>
2024-04-19 12:03:06 +0100

    fix: add navigate hook

--- src/hooks/useLogin.ts
+++ src/hooks/useLogin.ts
@@ -1,5 +1,6 @@
 import { useMutation } from "@tanstack/react-query";
 import axios from "axios";
+import { useNavigate } from "react-router-dom";
 import { Bounce, toast } from "react-toastify";
 
 interface User {
@@ -22,22 +23,19 @@ export const notify = (err: string) => {
 };
 
 const useLogin = () => {
+  const navigate = useNavigate();
   return useMutation({
     mutationFn: (user: User) => {
       return axios
         .post<User>(
           "https://digital-church.onrender.com/api/v1/users/login",
-          user,
-          {
-            withCredentials: true,
-          }
+          user
         )
         .then((res) => res.data);
     },
     onSuccess: () => {
       console.log("It works");
-      window.location.href =
-        "https://digital-church-web.vercel.app/personalinfo";
+      navigate('/personalinfo')
     },
     onError: () => {
       notify("Incorrect Username or Password");
