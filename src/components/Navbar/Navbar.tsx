386120e499dc40fb90cd1f56ab586d1a678f6cc1
Temidire <temidireowoeye@gmail.com>
2024-04-23 17:01:00 +0100

    fix

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,5 +1,4 @@
 import { Link } from "react-router-dom";
-import { TbVector } from "react-icons/tb";
 import { RxDashboard } from "react-icons/rx";
 import { BsPeople } from "react-icons/bs";
 import { FaWpforms } from "react-icons/fa6";
@@ -21,57 +20,64 @@ const Navbar: React.FC<NavBarProps> = ({ active }) => {
     {
       source: "../../../public/assets/images/Navbar/MembershipIcon.svg",
       title: "Dashboard",
-      icon: <RxDashboard className="text-3xl" />,
+      icon: <RxDashboard className="text-2xl" />,
       // route: `/admin/overview/dashboard/${churchId}`,
       route: `/admin/overview/dashboard/`,
     },
     {
       source: "../../../public/assets/images/Navbar/MembershipIcon.svg",
-      title: "Membership",
+      title: "Directory",
       route: `/admin/church/members`,
       // route: `/admin/church/${churchId}/members`,
-      icon: <BsPeople className="text-3xl" />,
+      icon: <BsPeople className="text-2xl" />,
     },
     {
       source: "../../../public/assets/images/Navbar/FormsIcon.svg",
       title: "Forms",
       route: "/overview/forms",
-      icon: <FaWpforms className="text-3xl" />,
+      icon: <FaWpforms className="text-2xl" />,
     },
     {
       source: "../../../public/assets/images/Navbar/WorkflowIcon.svg",
       title: "Workflow",
       route: "/overview/workflow",
-      icon: <GoWorkflow className="text-3xl" />,
+      icon: <GoWorkflow className="text-2xl" />,
     },
     {
       source: "../../../public/assets/images/Navbar/SettingsIcon.svg",
       title: "Settings",
       route: "/overview/settings",
-      icon: <IoSettingsOutline className="text-3xl" />,
+      icon: <IoSettingsOutline className="text-2xl" />,
     },
     {
       source: "../../../public/assets/images/Navbar/HelpIcon.svg",
       title: "Help",
       route: "/overview/help",
-      icon: <IoMdHelpCircleOutline className="text-3xl" />,
+      icon: <IoMdHelpCircleOutline className="text-2xl" />,
     },
     {
       source: "../../../public/assets/images/Navbar/LogoutIcon.svg",
       title: "Logout",
       route: "/overview/logout",
-      icon: <IoIosLogOut className="text-3xl" />,
+      icon: <IoIosLogOut className="text-2xl" />,
     },
   ];
 
   return (
-    <nav className="px-5 pt-16 inline-flex flex-col items-center bg-DarkBlue text-[#99A0B7] text-[18px] h-screen font-azo font-medium sticky top-0">
-      <TbVector className="text-3xl mb-6" />
+    <nav className="px-8 pt-16 inline-flex flex-col bg-DarkBlue text-[#99A0B7] text-[18px] h-screen font-azo font-medium sticky top-0">
+      <div className="flex items-center space-x-2 mb-10 pl-5 ">
+        <img
+          src="/assets/images/Membership.svg"
+          alt=""
+          className="bg-[#2D833F] p-2 rounded-lg"
+        />
+        <p>Membership</p>
+      </div>
       <ul className="gap-y-4 flex flex-col">
         {navList.map((item) => (
           <Link
             to={item.route}
-            className={` hover:bg-DarkBlueHover hover:text-white py-2 px-4 rounded-[10px] cursor-pointer ${
+            className={` hover:bg-DarkBlueHover hover:text-white py-2 pl-5 pr-10 rounded-[10px] cursor-pointer ${
               // location.pathname === item.route && "bg-DarkBlueHover text-white"
               active === item.title && "bg-DarkBlueHover text-white"
             }`}
