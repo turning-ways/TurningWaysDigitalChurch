c9dc80f6901bcac87c977e02eed546f1c9167870
Temidire <temidireowoeye@gmail.com>
2024-04-28 17:22:58 +0100

    fix: add back button and hid scrollbar

--- src/components/Container/AuthContainer.tsx
+++ src/components/Container/AuthContainer.tsx
@@ -1,3 +1,5 @@
+import { MdOutlineKeyboardArrowLeft } from "react-icons/md";
+
 interface AuthContainerProps {
   children: React.ReactNode[] | React.ReactNode;
   center?: string;
@@ -5,23 +7,31 @@ interface AuthContainerProps {
 
 const AuthContainer: React.FC<AuthContainerProps> = ({ children, center }) => {
   return (
-    <div className={`h-full overflow-y-scroll px-8 lg:px-0 flex ${center}`}>
-      <div className="flex flex-col py-10 w-full overflow-y-scroll max-w-[550px] mx-auto">
-        {children}{" "}
+    <div className="relative">
+      <div
+        className="bg-secondary fixed m-6 p-2 cursor-pointer rounded-2xl"
+        onClick={() => window.history.back()}
+      >
+        <MdOutlineKeyboardArrowLeft className="text-4xl " />
       </div>
-      {/* <footer className=" justify-between text-[#888888] w-full pb-3 flex text-sm items-center absolute bottom-0 px-8">
-        <div className="flex items-center space-x-2">
-          <img src="../../public/assets/images/Logo.svg" alt="" />
-          <p className="mx-0">&copy; 2024 </p>
+      <div className={`h-full overflow-y-scroll px-8 lg:px-0 flex ${center}`}>
+        <div className="flex flex-col py-10 w-full overflow-y-scroll max-w-[550px] mx-auto scrollbar-hide">
+          {children}{" "}
         </div>
-        <ul className="flex space-x-4 ">
-          <li className="hover:cursor-pointer">User Agreement</li>
-          <li className="hover:cursor-pointer">Privacy Policy</li>
-          <li className="hover:cursor-pointer">Copyright Policy</li>
-          <li className="hover:cursor-pointer">Cookie Policy</li>
-          <li className="hover:cursor-pointer">Support</li>
-        </ul>
-      </footer> */}
+        {/* <footer className=" justify-between text-[#888888] w-full pb-3 flex text-sm items-center absolute bottom-0 px-8">
+          <div className="flex items-center space-x-2">
+            <img src="../../public/assets/images/Logo.svg" alt="" />
+            <p className="mx-0">&copy; 2024 </p>
+          </div>
+          <ul className="flex space-x-4 ">
+            <li className="hover:cursor-pointer">User Agreement</li>
+            <li className="hover:cursor-pointer">Privacy Policy</li>
+            <li className="hover:cursor-pointer">Copyright Policy</li>
+            <li className="hover:cursor-pointer">Cookie Policy</li>
+            <li className="hover:cursor-pointer">Support</li>
+          </ul>
+        </footer> */}
+      </div>
     </div>
   );
 };
