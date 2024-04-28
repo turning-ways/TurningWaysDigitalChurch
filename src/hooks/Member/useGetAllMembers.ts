c1b04de7e49d27bee2dd72d097228a88e5a1e4a1
Temidire <temidireowoeye@gmail.com>
2024-04-28 17:24:51 +0100

    fix: moved files

---
+++ src/hooks/Member/useGetAllMembers.ts
@@ -0,0 +1,25 @@
+import { useQuery } from "@tanstack/react-query"
+import { useChurchIdStore } from "../../stores/churchId";
+import axios from "axios";
+
+const useGetAllMembers = () => {
+    const { churchId } = useChurchIdStore();
+
+
+  return useQuery({
+    queryKey: ["churches", churchId, "members"],
+    queryFn: () =>
+      axios
+        .get(
+          `https://digital-church.onrender.com/api/v1/churches/${churchId}/members`,
+          {
+            withCredentials: true,
+          }
+        )
+        .then((res) => res.data)
+        .then((data) => data.data.members)
+        .catch((err) => console.log(err)),
+  });
+}
+
+export default useGetAllMembers
---
+++ src/hooks/Signup/useVerifyOtp.ts
@@ -0,0 +1,28 @@
+import { useMutation } from "@tanstack/react-query";
+import axios from "axios";
+import { useUserIdStore } from "../../stores/user";
+import { useNavigate } from "react-router-dom";
+
+interface Otp {
+  token: string;
+}
+
+const useVerifyOtp = () => {
+  const { setUserId } = useUserIdStore();
+  const navigate = useNavigate();
+  return useMutation({
+    mutationFn: (token: Otp) =>
+      axios
+        .patch<Response>(
+          "https://digital-church.onrender.com/api/v1/users/verify-password-token",
+          token
+        )
+        .then((res) => res.data),
+    onSuccess: (res) => {
+      setUserId(res.userId);
+      navigate("/password-reset/set-new-password");
+    },
+  });
+};
+
+export default useVerifyOtp;
