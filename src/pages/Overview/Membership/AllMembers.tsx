cb068755f3c8dd8ebb6141982c1d0536845f7cde
Temidire <temidireowoeye@gmail.com>
2024-04-28 17:27:01 +0100

    fix

--- src/pages/Overview/Membership/AllMembers.tsx
+++ src/pages/Overview/Membership/AllMembers.tsx
@@ -1,25 +1,15 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import AddMemberBtn from "../AddMemberBtn";
 import { FaArrowRight } from "react-icons/fa";
 import { useNavigate } from "react-router-dom";
-import { useQuery } from "@tanstack/react-query";
 import { useChurchIdStore } from "../../../stores/churchId";
-import axios from "axios";
+import useGetAllMembers from "../../../hooks/Member/useGetAllMembers";
 
 const AllMembers = () => {
   const navigate = useNavigate();
   const { churchId } = useChurchIdStore();
 
-  const { data: member } = useQuery({
-    queryKey: ["churches", churchId, "members"],
-    queryFn: () =>
-      axios
-        .get(
-          `https://digital-church.onrender.com/api/v1/churches/${churchId}/members`,
-          { withCredentials: true }
-        )
-        .then((res) => res.data)
-        .catch((err) => console.log(err)),
-  });
+  const { data: members } = useGetAllMembers();
 
   return (
     <div>
@@ -34,12 +24,8 @@ const AllMembers = () => {
         <div className="">Gender</div>
       </div>
 
-      {churchId &&
-        member &&
-        member.data &&
-        member.data.members &&
-        // eslint-disable-next-line @typescript-eslint/no-explicit-any
-        member.data.members.map((item: any) => (
+      {churchId && members ? (
+        members.map((item: any) => (
           <div className="grid grid-cols-[100px,210px,280px,150px,150px,auto] border-b py-4  text-[#636363] gap-4">
             <div className="flex space-x-2 items-center">
               {/* <MdOutlineCheckBoxOutlineBlank className="text-xl" /> */}
@@ -54,14 +40,17 @@ const AllMembers = () => {
               className="flex items-center gap-x-2 text-secondary cursor-pointer"
               onClick={() => {
                 navigate(
-                  `/overview/membership/personal-information?id=${item._id}`
+                  `/admin/directory/member/personal-information?id=${item._id}`
                 );
               }}
             >
               <p>View more</p> <FaArrowRight />
             </div>
           </div>
-        ))}
+        ))
+      ) : (
+        <div>There's no member</div>
+      )}
       <AddMemberBtn />
     </div>
   );
