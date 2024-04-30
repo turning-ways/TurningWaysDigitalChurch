4160699c848b7fad3d43c530f37363c372cefb2f
Temidire <temidireowoeye@gmail.com>
2024-04-30 07:46:10 +0100

    fix: padding

--- src/components/Button/GoogleButton.tsx
+++ src/components/Button/GoogleButton.tsx
@@ -2,11 +2,11 @@ const GoogleButton = () => {
   return (
     <a
       // className="border border-[#CBD5E0] rounded-[20px] py-3 flex justify-center space-x-3 items-center w-full lg:max-w-[550px] cursor-pointer"
-      className="border border-[#CBD5E0] rounded-[20px] py-3 flex justify-center space-x-3 items-center w-full lg:max-w-[550px] cursor-pointer mt-5"
+      className="border border-[#CBD5E0] rounded-[20px] py-3 px-6 flex space-x-3 items-center w-full lg:max-w-[550px] cursor-pointer mt-5"
       href="https://digital-church.onrender.com/api/v1/users/auth/google/admin"
     >
       <img src="/assets/images/Google.svg" alt="" />
-      <p className=" text-center text-[#67728A] text-sm lg:text-md font-medium">
+      <p className=" text-center w-full text-[#67728A] text-lg font-medium">
         Continue with google
       </p>
     </a>
