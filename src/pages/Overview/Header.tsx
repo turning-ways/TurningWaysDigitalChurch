f74b92abfcebe8c3deaf48fe1f9845b66b329a13
Temidire <temidireowoeye@gmail.com>
2024-05-16 13:42:00 +0100

    fix

--- src/pages/Overview/Header.tsx
+++ src/pages/Overview/Header.tsx
@@ -7,6 +7,8 @@ import {
 import { useUserAuth } from "../../stores/user";
 import { useNavigate } from "react-router-dom";
 import useGetAllMembers from "../../hooks/Member/useGetAllMembers";
+import Modal from "../../components/Modal/Modal";
+import { success } from "../../hooks/useUpdatePassword";
 
 interface HeaderProps {
   text: string;
@@ -20,7 +22,7 @@ const Header: React.FC<HeaderProps> = ({ text }) => {
 
   const [value, setValue] = useState<string>("");
 
-  const { data: members } = useGetAllMembers({page:1, pageSize:10000});
+  const { data: members } = useGetAllMembers({ page: 1, pageSize: 10000 });
 
   const [showProfile, setShowProfile] = useState<boolean>(false);
 
@@ -31,6 +33,31 @@ const Header: React.FC<HeaderProps> = ({ text }) => {
       member.first_name.toLowerCase().includes(value.toLowerCase()) ||
       member.last_name.toLowerCase().includes(value.toLowerCase())
   );
+
+  const [active, setActive] = useState("");
+
+  const { setUser } = useUserAuth();
+
+  const logoutUser = async () => {
+    try {
+      const response = await fetch(
+        "https://digital-church.onrender.com/api/v1/users/logout",
+        {
+          method: "GET",
+          credentials: "include",
+        }
+      );
+
+      if (response.ok) {
+        setUser(null);
+        success("Logged Out");
+      } else {
+        console.error("Error logging out user:", response.statusText);
+      }
+    } catch (error) {
+      console.error("Error logging out user:", error);
+    }
+  };
 
   return (
     <div className="space-y-5 font-azo flex flex-col relative">
@@ -130,7 +157,29 @@ const Header: React.FC<HeaderProps> = ({ text }) => {
         </div>
         <h1>Profile</h1>
         <h1>Settings</h1>
-        <h1>Logout</h1>
+        <h1 className="cursor-pointer" onClick={() => setActive("logout")}>Logout</h1>
+        {active === "logout" && (
+          <Modal>
+            <div className="bg-white px-[26px] py-[37px] rounded-2xl text-lg flex flex-col gap-6">
+              <p>Are you sure you want to log out</p>
+
+              <div className="flex self-center space-x-4">
+                <button
+                  className=" text-[#ffffff] bg-[#e74a4a]  py-2 px-4 rounded-[14px] self-center"
+                  onClick={logoutUser}
+                >
+                  Logout
+                </button>
+                <button
+                  className=" text-[#7B7B7B] bg-[#F4F4F4] py-2 px-4 rounded-[14px] self-center"
+                  onClick={() => setActive("")}
+                >
+                  Cancel
+                </button>
+              </div>
+            </div>
+          </Modal>
+        )}
       </div>
     </div>
   );
