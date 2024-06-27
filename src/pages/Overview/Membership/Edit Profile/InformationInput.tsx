b970f82ad0be8cca8992a17f7c8e179b57f903db
Temidire <temidireowoeye@gmail.com>
2024-06-27 15:58:45 +0100

    fix

--- src/pages/Overview/Membership/Edit Profile/InformationInput.tsx
+++ src/pages/Overview/Membership/Edit Profile/InformationInput.tsx
@@ -4,17 +4,18 @@ interface InformationInputProps {
   text: string;
   value?: string;
   notCompulsory?:string;
+  type?:string;
   onChange?: (e: ChangeEvent<HTMLInputElement>) => void;
 }
 
-const InformationInput: React.FC<InformationInputProps> = ({ text, onChange, value, notCompulsory}) => {
+const InformationInput: React.FC<InformationInputProps> = ({ text, onChange, value, notCompulsory, type}) => {
   return (
     <div className=" space-y-1 mb-4 bg-white">
       <p className="text-[#727272]">
         {text} <span className="text-[#61BD74]">{notCompulsory ? notCompulsory : "*"}</span>
       </p>
       <div className="border rounded-lg p-2">
-        <input className="outline-none text-[#434343] text-lg w-full" onChange={onChange} value={value}/>
+        <input type={type} className="outline-none text-[#434343] text-lg w-full" onChange={onChange} value={value}/>
       </div>
     </div>
   );
