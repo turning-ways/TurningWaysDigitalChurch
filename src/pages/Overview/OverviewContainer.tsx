aee3f2521089a00a5a94ba2806441313e467449a
Temidire <temidireowoeye@gmail.com>
2024-06-27 14:51:40 +0100

    fix: spacing

--- src/pages/Overview/OverviewContainer.tsx
+++ src/pages/Overview/OverviewContainer.tsx
@@ -13,7 +13,7 @@ const OverviewContainer: React.FC<OverviewContainerProps> = ({
   return (
     <div className="lg:flex relative lg:mx-auto">
       <Navbar active={active} />
-      <div className=" p-5 pt-20 sm:p-10 sm:pt-20 md:pt-16 flex-grow font-azo h-screen overflow-y-scroll bg-[#FFFDFD] flex flex-col ">
+      <div className=" p-5 pt-[58px] sm:p-10 sm:pt-20 md:pt-16 flex-grow font-azo h-screen overflow-y-scroll bg-[#FFFDFD] flex flex-col ">
         {children}
       </div>
     </div>
